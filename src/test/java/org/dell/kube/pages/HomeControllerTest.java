1fa2f8a4349c41a65bc19d79bcd69242d8d06533
maremilan <mare.milan@gmail.com>
2020-12-15 08:27:31 +0000

    Added externalized config, config maps.

--- src/test/java/org/dell/kube/pages/HomeControllerTest.java
+++ src/test/java/org/dell/kube/pages/HomeControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HomeControllerTest {
+
     @Test
     public void itSaysYellowPagesHello() throws Exception {
         HomeController controller = new HomeController("YellowPages");
